e1767710420472787ad1cfda34ac120f7b1d2fa1
lionelIntellij <lionel.lacroix3@gmail.com>
2019-02-13 01:11:23 +0100

    add Image

--- sources/person/ImagePerson.cpp
+++ sources/person/ImagePerson.cpp
@@ -1,6 +1,8 @@
 
 #include "ImagePerson.h"
 #include "Image.h"
+#include <QImage>
+#include "Detector.h"
 
 ImagePerson::ImagePerson() : ImageWidget() {
 
@@ -10,10 +12,9 @@ ImagePerson::ImagePerson() : ImageWidget() {
 }
 
 void ImagePerson::selectPerson_on_clicked() {
-  Image image(myImage);
-
-  setImage();
-
+  Image image(&myImage);
+  Detector detector(&image);
+  setImage((*detector.getSegmentedImage()));
 }
 
 ImagePerson::~ImagePerson() = default;
